81e5c21cf083248470b55a42d28b0d8505901d6b
Beau Elliott <belliott1516@gmail.com>
2022-05-04 15:02:57 -0700

    completed handleDeleteGoblin and currently working

--- src/App.js
+++ src/App.js
@@ -13,15 +13,11 @@ function App() {
       goblinFormHP, which is how we track the user input for the current HP of the goblin in the form
       goblinFormColor, which is how we track the user input for the current color of the goblin in the form
 */
-  const [allGoblins, setAllGoblins] = useState([{
-    name: 'tyler',
-    hp: 15,
-    color: 'green'
-  }]);
-  const [filteredGoblins, setFilteredGoblins] = useState();
-  const [goblinFormName, setGoblinFormName] = useState('Ralph');
-  const [goblinFormHP, setGoblinFormHP] = useState(10);
-  const [goblinFormColor, setGoblinFormColor] = useState('orange');
+  const [allGoblins, setAllGoblins] = useState([]);
+  const [filteredGoblins, setFilteredGoblins] = useState(allGoblins);
+  const [goblinFormName, setGoblinFormName] = useState('');
+  const [goblinFormHP, setGoblinFormHP] = useState('');
+  const [goblinFormColor, setGoblinFormColor] = useState('green');
   
   function submitGoblin(e) {
     e.preventDefault();
@@ -37,22 +33,24 @@ function App() {
     // clear out the goblin form state items by setting them to empty strings. This will cause the form to reset in the UI.
     setGoblinFormName('');
     setGoblinFormHP('');
-    setGoblinFormColor('');
+    setGoblinFormColor('orange');
   }
 
   function handleDeleteGoblin(name) {
     // find the index of the goblin in allGoblins with this name
-
+    const goblinIndex = allGoblins.findIndex((goblin) => goblin.name === name);
     // use splice to delete the goblin object at this index
-
+    allGoblins.splice(goblinIndex, 1);
     // update the allGoblins array immutably to this new, smaller array
+    setAllGoblins([...allGoblins]);
   }
 
   function handleFilterGoblins(search) {
     // use the filter method to get an array of goblins whose name includes this search argument
-
+    const goblinSearch = allGoblins.filter(goblin => goblin.name.toLowerCase().includes(search.toLowerCase()));
     // if there is a search argument, set the filtered goblins to the filtered goblins
     // if the search argument is undefined, set the filtered goblins in state to just be the array of all goblins
+    setFilteredGoblins([...goblinSearch]);
   }
 
 
